e81cc0d94fd616df291f61c6714d99adc0377ad3
Olsen Lila <olsenlila8@gmail.com>
2022-11-05 11:28:34 +0100

    new links

--- pages/urls.py
+++ pages/urls.py
@@ -5,4 +5,10 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("blog/", views.blog, name="blog"),
+    path("about-us/", views.about_us, name="about-us"),
+    path("contact-us/", views.contact_us, name="contact-us"),
+    path("privacy-policy/", views.privacy_policy, name="privacy-policy"),
+    path("bestnewdestinations/", views.bestnewdestinations, name="bestnewdestinations"),
+    path("booktrip/", views.booktrip, name="Book & Trip"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,33 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "pages/home.html")
+
+
+def blog(request):
+    return render(request, "pages/blog.html")
+
+
+def about_us(request):
+    return render(request, "pages/about-us.html")
+
+def contact_us(request):
+    return render(request, "pages/contact-us.html")
+
+def privacy_policy(request):
+    return render(request, "pages/privacy-policy.html")
+
+
+def bestnewdestinations(request):
+    return render(request, "pages/bestnewdestinations.html")
+
+def booktrip(request):
+    return render(request, "pages/booktrip.html")
+
+
+
+
+
+
+
+
+
